17ab0c2ad2d5d961a5aa8dc2b084a65d5c13fd8f
lliangzeyuan-wq <lliangzeyuan@gmail.com>
2026-02-16 14:21:45 +0800

    添加项目文件。

---
+++ ThreadPool/ThreadPool.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<thread>
+#include<string>
+void printHelloWorld(std::string msg) {
+	std::cout << msg << std::endl;  
+	return;
+}
+void printLZY(std::string msg) {
+	for (int i = 0; i < 10000; i++) {
+		std::cout << i;
+	}
+	std::cout << std::endl;
+}
+void test1() {
+	std::thread thread1(printHelloWorld,"hejasiud dfsaj");
+	//thread1.join();
+	//thread1.detach();
+	bool isJoin = thread1.joinable();
+	if (isJoin) {
+		thread1.join();
+	}
+}
+void test2() {
+	std::thread thread1(printLZY, "hejasiud dfsaj");
+	//thread1.join();
+	//thread1.detach();
+	bool isJoin = thread1.joinable();
+	if (isJoin) {
+		thread1.join();
+	}
+	std::cout << "over" << std::endl;
+}
+int main() {
+	test2();
+	return 0;
+}
