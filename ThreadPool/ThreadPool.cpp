7230564765b320b6af912aa982d35e8be984514e
lliangzeyuan-wq <lliangzeyuan@gmail.com>
2026-02-17 00:39:55 +0800

    互斥量解决多线程数据共享问题

--- ThreadPool/ThreadPool.cpp
+++ ThreadPool/ThreadPool.cpp
@@ -1,36 +1,19 @@
 ﻿#include<iostream>
 #include<thread>
-#include<string>
-void printHelloWorld(std::string msg) {
-	std::cout << msg << std::endl;  
-	return;
-}
-void printLZY(std::string msg) {
-	for (int i = 0; i < 10000; i++) {
-		std::cout << i;
+int a = 0;
+void func() {
+	for (int i = 0; i < 10000; i++)
+	{
+		a += 1;
 	}
-	std::cout << std::endl;
-}
-void test1() {
-	std::thread thread1(printHelloWorld,"hejasiud dfsaj");
-	//thread1.join();   //等子进程结束再结束main函数
-	//thread1.detach();   //允许main函数结束，子进程在后台运行
-	bool isJoin = thread1.joinable();
-	if (isJoin) {
-		thread1.join();
-	}
-}
-void test2() {
-	std::thread thread1(printLZY, "hejasiud dfsaj");
-	//thread1.join();
-	//thread1.detach();
-	bool isJoin = thread1.joinable();
-	if (isJoin) {
-		thread1.join();
-	}
-	std::cout << "over" << std::endl;
 }
 int main() {
-	test2();
+	std::thread t1(func);
+	std::thread t2(func);
+	
+	t1.join();
+	t2.join();
+	
+	std::cout << a << std::endl;
 	return 0;
 }
