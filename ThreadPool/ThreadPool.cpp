1f140d8f5f99477974fb0b79274d26b75dc1b9ed
lliangzeyuan-wq <lliangzeyuan@gmail.com>
2026-02-20 21:43:13 +0800

    视频链接：https://www.bilibili.com/video/BV1uE421F7ZK/?spm_id_from=333.337.search-card.all.click

--- ThreadPool/ThreadPool.cpp
+++ ThreadPool/ThreadPool.cpp
@@ -1,36 +1,59 @@
 ﻿#include<iostream>
 #include<thread>
-#include<string>
-void printHelloWorld(std::string msg) {
-	std::cout << msg << std::endl;  
-	return;
-}
-void printLZY(std::string msg) {
-	for (int i = 0; i < 10000; i++) {
-		std::cout << i;
+#include<mutex>
+#include<condition_variable>
+#include<queue>
+
+const int QUEUE_THRESHHOLD = 10;
+std::mutex mtx;
+std::condition_variable cv;
+
+class Queue {
+public:
+	void put(int val) {
+		std::unique_lock<std::mutex>lock(mtx);
+		//while (q.size() > QUEUE_THRESHHOLD) {
+		//	cv.wait(lock);
+		//}
+		cv.wait(lock, [&]()->bool {return q.size() <= QUEUE_THRESHHOLD; });
+		q.push(val);
+		std::cout << "producer:" << val << std::endl;
+		cv.notify_all();
 	}
-	std::cout << std::endl;
-}
-void test1() {
-	std::thread thread1(printHelloWorld,"hejasiud dfsaj");
-	//thread1.join();
-	//thread1.detach();
-	bool isJoin = thread1.joinable();
-	if (isJoin) {
-		thread1.join();
+	
+	int get() {
+		std::unique_lock<std::mutex>lock(mtx);
+		//while (q.empty()) {
+		//	cv.wait(lock);
+		//}
+		cv.wait(lock, [&]()->bool {return !q.empty(); });
+		int val = q.front();
+		q.pop();
+		std::cout << "consumer:" << val << std::endl;
+		cv.notify_all();
+		return val;
+	}
+private:
+	std::queue<int>q;
+};
+
+void producer(Queue* q) {
+	for (int i = 1; i <= 100; i++) {
+		q->put(i);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 }
-void test2() {
-	std::thread thread1(printLZY, "hejasiud dfsaj");
-	//thread1.join();
-	//thread1.detach();
-	bool isJoin = thread1.joinable();
-	if (isJoin) {
-		thread1.join();
+
+void consumer(Queue* q) {
+	for (int i = 1; i <= 100; i++) {
+		q->get();
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
-	std::cout << "over" << std::endl;
 }
 int main() {
-	test2();
-	return 0;
+	Queue q;
+	std::thread t1(producer, &q);
+	std::thread t2(consumer, &q);
+	t1.join();
+	t2.join();
 }
