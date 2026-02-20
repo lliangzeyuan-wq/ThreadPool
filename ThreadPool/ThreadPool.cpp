440c1685bfd3b7598446d299420900f3f8fc759a
lliangzeyuan-wq <lliangzeyuan@gmail.com>
2026-02-20 18:00:02 +0800

    call_once的使用

--- ThreadPool/ThreadPool.cpp
+++ ThreadPool/ThreadPool.cpp
@@ -1,23 +1,40 @@
 ﻿#include<iostream>
-#include<thread>
+using namespace std;
 #include<mutex>
-int a = 0;
-std::mutex mtx;
-void func() {
-	for (int i = 0; i < 10000; i++)
-	{
-		mtx.lock();
-		a += 1;
-		mtx.unlock();
+once_flag m_flag;
+
+class TaskQueue {
+	friend void init();
+public:
+	TaskQueue(const TaskQueue& t) = delete;
+	TaskQueue& operator=(const TaskQueue& t) = delete;
+	static TaskQueue* getInstance() {
+		call_once(m_flag, [&] {
+			m_taskQ = new TaskQueue;
+			});
+		return m_taskQ;
 	}
-}
+	void print() {
+		cout << "我是单例对象的一个成员函数..." << endl;
+	}
+private:
+	//TaskQueue() {};
+	//TaskQueue(const TaskQueue& t) {};
+	//TaskQueue& operator=(const TaskQueue& t) {};
+
+	TaskQueue() = default;
+	//TaskQueue(const TaskQueue& t) = default;
+	//TaskQueue& operator=(const TaskQueue& t) = default;
+
+	//只能通过类名访问静态属性或方法
+	static TaskQueue* m_taskQ;
+};
+TaskQueue* TaskQueue::m_taskQ = nullptr;
+
+
+
 int main() {
-	std::thread t1(func);
-	std::thread t2(func);
-
-	t1.join();
-	t2.join();
-
-	std::cout << a << std::endl;
+	TaskQueue* taskQ = TaskQueue::getInstance();
+	taskQ->print();
 	return 0;
 }
